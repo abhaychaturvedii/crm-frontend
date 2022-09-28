3e2595535db7a1dbb28563cb7936d334040e1682
abhaychaturvedii <55993241+abhaychaturvedii@users.noreply.github.com>
2022-09-28 16:32:13 +0530

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Entry } from "./page/entry/entry.page";
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+
+      <DefaultLayout>
+       //Dashboard
+      </DefaultLayout>
     </div>
   );
 }
